37adc853767f8ac27e63411835bbabd77f52d76f
Alison de Miranda Peres <alison@rc.local>
2022-08-29 15:51:07 -0300

    Adding Card Class

---
+++ MarvelSnapApi/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace MarvelSnapApi
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Text { get; set; } = String.Empty;
+        public int Cost { get; set; }
+        public int Power { get; set; }
+
+    }
+}

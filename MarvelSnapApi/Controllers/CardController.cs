7647b8a8db070ed28d9019dff18d239b78eaa521
Alison de Miranda Péres <alison098@hotmail.com>
2022-09-14 01:36:07 -0300

    fix: removing array

--- MarvelSnapApi/Controllers/CardController.cs
+++ MarvelSnapApi/Controllers/CardController.cs
@@ -8,17 +8,6 @@ namespace MarvelSnapApi.Controllers
     public class CardController : ControllerBase
     {
 
-        private static List<Card> cards = new List<Card>
-            {
-                new Card
-                {
-                    Id = 1, Name = "Doctor Octopus",
-                    Text = "On Reveal: Pull 4 random cards from your opponent's hand to their side of this location.",
-                    Cost = 5,
-                    Power = 10,
-                    Image = "imagem1.jpg"
-                }
-            };
         private readonly DataContext _context;
 
         public CardController(DataContext context)

883409ea9e79bfbde40dc69f2f40e5ab799157e5
Alison de Miranda Péres <alison098@hotmail.com>
2022-09-13 22:35:52 -0300

    DataContext

--- MarvelSnapApi/Data/DataContext.cs
+++ MarvelSnapApi/Data/DataContext.cs
@@ -4,5 +4,8 @@ namespace MarvelSnapApi.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Card> Cards { get; set; }
     }
 }

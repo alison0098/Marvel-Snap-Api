658b97e75ad01bef823a36121b4a8a53f9fdb149
Alison de Miranda Péres <alison098@hotmail.com>
2022-09-13 23:50:35 -0300

    add: dbcontext service to container

--- MarvelSnapApi/Program.cs
+++ MarvelSnapApi/Program.cs
@@ -1,8 +1,16 @@
+global using MarvelSnapApi.Data;
+global using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
